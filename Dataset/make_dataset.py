c51df71c423032a379c8576a8edeb26c99049f66
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2024-02-27 12:51:23 +0100

    Making the Datapack (2/2)

--- Dataset/make_dataset.py
+++ Dataset/make_dataset.py
@@ -3,13 +3,14 @@ from utils import get_pose
 
 import os
 import sys
+import shutil
 import pandas as pd
 from tqdm import tqdm
 from time import sleep
 from datetime import datetime
 import mcpi.minecraft as minecraft
 
-out_dir = 'output/run_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+out_dir = 'output/run_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '/'
 os.makedirs(out_dir)
 
 mc = minecraft.Minecraft.create()
@@ -33,5 +34,13 @@ mc.postToChat('stop')
 
 # Save Poses
 poses = pd.DataFrame(poses)
-poses.to_csv('out_dir/poses.csv')
+poses.to_csv(out_dir + 'poses.csv', index=False)
 print(poses)
+
+# Make Datapack
+shutil.copytree('datapack_template/v2s_dataset_datapack', out_dir + 'v2s_dataset_datapack')
+with open(out_dir + 'v2s_dataset_datapack/data/v2s/functions/tick.mcfunction', 'w') as f:
+    for i in range(len(poses)):
+        p = poses.iloc[i]
+        tp = f'tp @a[scores={{v2s_rclick={i+1}}}] {p.x} {p.y} {p.z} {p.rot} {p.pit}\n'
+        f.write(tp)
