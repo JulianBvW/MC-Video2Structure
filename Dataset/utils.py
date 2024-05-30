2c7fb297b176ffbbf1f6a3b6820c2bbb9b8d1561
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2024-05-30 12:35:59 +0200

    Added cyclic representation functions

--- Dataset/utils.py
+++ Dataset/utils.py
@@ -1,3 +1,5 @@
+import math
+
 def get_pose(mc):
     position_rel = mc.player.getPos()
     position_abs = position_rel.x-SPAWN_OFFSET[0], position_rel.y-SPAWN_OFFSET[1], position_rel.z-SPAWN_OFFSET[2]
@@ -16,3 +18,17 @@ def get_pose(mc):
     return pose
 
 SPAWN_OFFSET = (-8, -4, -8)
+
+def rot_to_sin_cos_repr(rot):
+    rot += 180
+    rot %= 90
+    rot *= 4
+    rot = math.radians(rot)
+    return math.sin(rot), math.cos(rot)
+
+def sin_cos_repr_to_rot(sin_value, cos_value):
+    rot = math.atan2(sin_value, cos_value)
+    rot = math.degrees(rot)
+    rot /= 4
+    rot += 90 if rot < 0 else 0
+    return rot
